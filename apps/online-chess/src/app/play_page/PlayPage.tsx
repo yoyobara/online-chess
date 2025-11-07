3086fc3afa32b29159b4cd9f5ca464e06de45046
Yotam Barak <yotambarak6@gmail.com>
2025-11-07 20:14:10 +0200

    simple player paper

--- apps/online-chess/src/app/play_page/PlayPage.tsx
+++ apps/online-chess/src/app/play_page/PlayPage.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import styles from './PlayPage.module.scss';
 import { Button } from '../../components/Button/Button';
 import { Paper } from '../../components/Paper/Paper';
+import { PlayerPaper } from './player_paper/PlayerPaper';
 
 export const PlayPage: FC = () => {
   return (
@@ -9,8 +10,18 @@ export const PlayPage: FC = () => {
       <div className={styles.board_container}></div>
       <Paper className={styles.chat}></Paper>
       <Paper className={styles.history}></Paper>
-      <Paper className={styles.player}></Paper>
-      <Paper className={styles.opponent}></Paper>
+      <PlayerPaper
+        playerName="me"
+        playerRating={1000}
+        variant="white"
+        className={styles.player}
+      />
+      <PlayerPaper
+        playerName="foe"
+        playerRating={200}
+        variant="purple"
+        className={styles.opponent}
+      />
       <div className={styles.buttons}>
         <Button variant="black">Resign</Button>
         <Button variant="white">Offer Draw</Button>
