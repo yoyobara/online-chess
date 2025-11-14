9b8b8d7a72dd8c1925419b159e9affa9c81a1a32
Yotam Barak <yotambarak6@gmail.com>
2025-11-14 02:42:32 +0200

    secret fix

--- apps/backend/src/extractors/auth.rs
+++ apps/backend/src/extractors/auth.rs
@@ -3,7 +3,7 @@ use jsonwebtoken::{decode, Algorithm, DecodingKey, Validation};
 use serde::{Deserialize, Serialize};
 use tower_cookies::Cookies;
 
-use crate::constants::auth::AUTH_COOKIE_NAME;
+use crate::{config::CONFIG, constants::auth::AUTH_COOKIE_NAME};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct AuthUser {
@@ -36,10 +36,9 @@ where
             .map(|c| c.value().to_string())
             .ok_or((StatusCode::UNAUTHORIZED, "missing jwt cookie"))?;
 
-        let secret = "mysecret";
         let decoded = decode::<Claims>(
             &jwt_token,
-            &DecodingKey::from_secret(secret.as_ref()),
+            &DecodingKey::from_secret(CONFIG.jwt_secret.as_ref()),
             &Validation::new(Algorithm::HS256),
         )
         .map_err(|_| (StatusCode::UNAUTHORIZED, "invalid jwt"))?;
