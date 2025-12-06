b8ecc351f63e53900364bc7ff088c9dabbe28970
Yotam Barak <yotambarak6@gmail.com>
2025-12-06 22:45:34 +0200

    looking for match modal

--- apps/online-chess/src/app/matchmaking_modal/MatchmakingModal.tsx
+++ apps/online-chess/src/app/matchmaking_modal/MatchmakingModal.tsx
@@ -1,10 +1,13 @@
 import { FC } from 'react';
 import { Modal } from '../../components/Modal/Modal';
+import styles from './MatchmakingModal.module.scss';
+import loading_spinner from '../../assets/loading_spinner.svg';
 
 export const MatchmakingModal: FC = () => {
   return (
-    <Modal>
-      <p>what</p>
+    <Modal modalClassName={styles.matchmaking_modal}>
+      <div>Looking For Match...</div>
+      <img className={styles.loading_spinner} src={loading_spinner} alt="..." />
     </Modal>
   );
 };
