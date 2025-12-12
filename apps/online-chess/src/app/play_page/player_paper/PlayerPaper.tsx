76a07299bcdd2d97a8eabe6c8854dc6b450398df
Yotam Barak <yotambarak6@gmail.com>
2025-12-12 21:19:22 +0200

    profile photo in player card

--- apps/online-chess/src/app/play_page/player_paper/PlayerPaper.tsx
+++ apps/online-chess/src/app/play_page/player_paper/PlayerPaper.tsx
@@ -2,6 +2,8 @@ import { FC } from 'react';
 import { Paper } from '../../../components/Paper/Paper';
 import styles from './PlayerPaper.module.scss';
 
+import profile_default from '../../../assets/profile_default.svg';
+
 interface PlayPaperProps {
   className?: string;
   variant: 'white' | 'purple';
@@ -17,6 +19,7 @@ export const PlayerPaper: FC<PlayPaperProps> = ({
 }) => {
   return (
     <Paper className={`${styles.player_paper} ${styles[variant]} ${className}`}>
+      <img className={styles.profile_photo} src={profile_default} alt="" />
       <div className={styles.player_name}>{playerName}</div>
       <div className={styles.player_rating}>Rating: {playerRating}</div>
     </Paper>
