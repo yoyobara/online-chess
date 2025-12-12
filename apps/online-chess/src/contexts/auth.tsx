76debd92c8e631273ec9ac401d5f49a5389b4eae
Yotam Barak <yotambarak6@gmail.com>
2025-12-12 22:08:45 +0200

    fixed authentication bug

--- apps/online-chess/src/contexts/auth.tsx
+++ apps/online-chess/src/contexts/auth.tsx
@@ -9,13 +9,17 @@ interface AuthData {
 const authContext = createContext<AuthData | null>(null);
 
 export const AuthProvider: FC<PropsWithChildren> = ({ children }) => {
-  const { data } = useQuery<AuthData>({
+  const { data, isLoading } = useQuery<AuthData>({
     queryKey: ['auth_data'],
     queryFn: () =>
       fetch('/api/auth/me', { credentials: 'include' }).then((resp) =>
         resp.json()
       ),
   });
+
+  if (isLoading) {
+    return null;
+  }
 
   return (
     <authContext.Provider value={data ?? null}>{children}</authContext.Provider>
