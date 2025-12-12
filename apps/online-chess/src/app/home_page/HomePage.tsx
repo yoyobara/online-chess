f6d402f893652074b425740a533e0bf26148793a
Yotam Barak <yotambarak6@gmail.com>
2025-12-12 22:43:51 +0200

    almost finished home page looks

--- apps/online-chess/src/app/home_page/HomePage.tsx
+++ apps/online-chess/src/app/home_page/HomePage.tsx
@@ -4,13 +4,23 @@ import { Button } from '../../components/Button/Button';
 import { useNavigate } from 'react-router-dom';
 import { Paper } from '../../components/Paper/Paper';
 
+import profile_default from '../../assets/profile_default.svg';
+import trophy_icon from '../../assets/trophy.svg';
+
+import { useRequiredAuth } from '../../contexts/auth';
+
 export const HomePage: FC = () => {
-  const navigate = useNavigate();
+  const auth = useRequiredAuth();
 
   return (
     <div className={styles.home_page}>
-      <Paper>
-        <button>yay</button>
+      <Paper className={styles.main_paper}>
+        <img className={styles.profile_photo} src={profile_default} alt="" />
+        <div className={styles.username}>{auth.username}</div>
+        <Paper className={styles.rank_paper}>
+          <img className={styles.trophy_icon} src={trophy_icon} alt="trophy" />
+          <div className={styles.rank_text}>4234</div>
+        </Paper>
       </Paper>
     </div>
   );
