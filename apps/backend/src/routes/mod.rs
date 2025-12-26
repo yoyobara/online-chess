88628d8b55b0a0c53b99ccd5ce5bef0e7adfc81f
Yotam Barak <yotambarak6@gmail.com>
2025-12-26 03:07:44 +0200

    front to back reverse proxy setup + axum logging

--- apps/backend/src/routes/mod.rs
+++ apps/backend/src/routes/mod.rs
@@ -3,12 +3,17 @@ pub mod ws;
 
 use axum::{routing::any, Router};
 use tower_cookies::CookieManagerLayer;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber;
 
 use crate::{routes::ws::ws_handler, state::AppState};
 
 pub fn router() -> Router<AppState> {
+    tracing_subscriber::fmt::init();
+
     Router::new()
         .nest("/api", api::router())
         .route("/realtime", any(ws_handler))
         .layer(CookieManagerLayer::new())
+        .layer(TraceLayer::new_for_http())
 }
