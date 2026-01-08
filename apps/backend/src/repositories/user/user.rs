8305208e0c15322729f59107f8db124ba225b9ea
Yotam Barak <yotambarak6@gmail.com>
2026-01-08 21:48:20 +0200

    use result type in user repo

--- apps/backend/src/repositories/user/user.rs
+++ apps/backend/src/repositories/user/user.rs
@@ -2,16 +2,16 @@ use std::fmt::Debug;
 
 use async_trait::async_trait;
 
-use crate::{models::user::UserData, repositories::user::UserRepositoryError};
+use crate::{models::user::UserData, repositories::user::error::UserRepositoryResult};
 
 #[async_trait]
 pub trait UserRepository: Send + Sync + Debug {
-    async fn get_user(&self, id: i32) -> Result<UserData, UserRepositoryError>;
-    async fn get_by_email(&self, email: String) -> Result<UserData, UserRepositoryError>;
+    async fn get_user(&self, id: i32) -> UserRepositoryResult<UserData>;
+    async fn get_by_email(&self, email: String) -> UserRepositoryResult<UserData>;
     async fn create_user(
         &self,
         username: String,
         email: String,
         password_hash: String,
-    ) -> Result<i32, UserRepositoryError>;
+    ) -> UserRepositoryResult<i32>;
 }
