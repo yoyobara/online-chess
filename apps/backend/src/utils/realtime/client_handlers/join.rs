df32bd71276ebc8bc7cd23c361ff47ea210d8540
Yotam Barak <yotambarak6@gmail.com>
2026-01-26 22:40:05 +0200

    client join handling

--- apps/backend/src/utils/realtime/client_handlers/join.rs
+++ apps/backend/src/utils/realtime/client_handlers/join.rs
@@ -1,10 +1,20 @@
-use crate::utils::realtime::RealtimeSession;
+use crate::utils::realtime::{client_communication::message::ServerMessage, RealtimeSession};
 
 pub async fn handle_client_join(session: &mut RealtimeSession) -> anyhow::Result<()> {
+    let match_state = session
+        .app_state
+        .match_repo
+        .get_match_state(&session.match_id)
+        .await?;
+
     session
         .app_state
         .match_repo
         .set_player_connected(&session.match_id, session.player_id, true)
+        .await?;
+
+    session
+        .communicator
+        .send(ServerMessage::MatchState(match_state))
         .await
-        .map_err(Into::into)
 }
