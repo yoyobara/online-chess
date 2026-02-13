737c5c9bece5b31a225ce381ef49dad4e86968a7
Yotam Barak <yotambarak6@gmail.com>
2026-02-13 14:57:41 +0200

    move check

--- apps/backend/src/utils/realtime/client_handlers/player_move.rs
+++ apps/backend/src/utils/realtime/client_handlers/player_move.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use rust_chess::core::chess_move::Move;
 use rust_chess::core::chess_move::Move as ChessMove;
 
 use crate::utils::{
@@ -13,38 +14,43 @@ pub async fn handle_client_player_move(
     session: &mut RealtimeSession,
     move_data: PlayerMoveData,
 ) -> Result<()> {
+    let mv = ChessMove::new(move_data.src_square, move_data.dest_square, None, None);
+
     let mut match_state = session
         .app_state
         .match_repo
         .get_match_state(&session.match_id)
         .await?;
 
-    match_state.board.apply_move(ChessMove::new(
-        move_data.src_square,
-        move_data.dest_square,
-        None,
-        None,
-    ));
-    match_state.move_count += 1;
+    let moves = match_state.board.get_legal_moves(mv.from).unwrap();
 
-    session
-        .communicator
-        .send(ServerMessage::MoveResult(true))
-        .await?;
+    if moves.contains(&mv) {
+        match_state.board.apply_move(mv);
+        match_state.move_count += 1;
 
-    session
-        .app_state
-        .match_repo
-        .update_match_state(&session.match_id, &match_state)
-        .await?;
+        session
+            .communicator
+            .send(ServerMessage::MoveResult(true))
+            .await?;
 
-    session
-        .pubsub
-        .publish(
-            &format!("match:{}", session.match_id),
-            &PubSubMessage::PlayerMove(match_state),
-        )
-        .await?;
+        session
+            .app_state
+            .match_repo
+            .update_match_state(&session.match_id, &match_state)
+            .await?;
 
+        session
+            .pubsub
+            .publish(
+                &format!("match:{}", session.match_id),
+                &PubSubMessage::PlayerMove(match_state),
+            )
+            .await?;
+    } else {
+        session
+            .communicator
+            .send(ServerMessage::MoveResult(false))
+            .await?;
+    }
     Ok(())
 }
