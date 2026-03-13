0b1b495e62142ac5dd2da727f3bf4a1f6dc5a396
Yotam Barak <yotambarak6@gmail.com>
2026-03-14 00:01:04 +0200

    updated rust-chess version to resolve stack overflow bug, modified test board

--- apps/backend/src/testboard.rs
+++ apps/backend/src/testboard.rs
@@ -6,10 +6,10 @@ use rust_chess::core::square::Square;
 pub fn get_test_board() -> Board {
     let mut b = Board::empty();
 
-    b.set(Square::A1, Some(Piece::new(PieceType::King, Color::White)));
-    b.set(Square::H8, Some(Piece::new(PieceType::King, Color::Black)));
-    b.set(Square::G6, Some(Piece::new(PieceType::Queen, Color::Black)));
-    b.set(Square::D6, Some(Piece::new(PieceType::Pawn, Color::White)));
+    b.set(Square::E1, Some(Piece::new(PieceType::King, Color::White)));
+    b.set(Square::E8, Some(Piece::new(PieceType::King, Color::Black)));
+    b.set(Square::H1, Some(Piece::new(PieceType::Rook, Color::White)));
+    b.set(Square::A8, Some(Piece::new(PieceType::Rook, Color::Black)));
 
     b
 }
