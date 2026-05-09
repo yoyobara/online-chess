7e1487d1c33eb62c7625ac5697c852bb6c3e24bb
Yotam Barak <yotambarak6@gmail.com>
2026-05-09 20:44:15 +0300

    bug fix

--- apps/online-chess/src/utils/square.ts
+++ apps/online-chess/src/utils/square.ts
@@ -6,10 +6,11 @@ export type File = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7;
 export const getRank = (index: number): Rank => Math.floor(index / 8) as Rank;
 export const getFile = (index: number): File => (index % 8) as File;
 
-export const fromCoords = (rank: number, file: number): number => rank * 8 + file;
+export const fromCoords = (rank: number, file: number): number =>
+  rank * 8 + file;
 
 export const fromAlgebraic = (name: string): number => {
-  const file = name.toLowerCase().charCodeAt(0) - 'a'.charCodeAt(0);
+  const file = name.toLowerCase().charCodeAt(0) - 'A'.charCodeAt(0);
   const rank = parseInt(name[1], 10) - 1;
   return fromCoords(rank, file);
 };
@@ -19,7 +20,7 @@ export const getSquareName = (index: number): string => {
   const file = getFile(index);
 
   const rankName = (rank + 1).toString();
-  const fileName = 'abcdefgh'.at(file);
+  const fileName = 'ABCDEFGH'.at(file);
 
   return `${fileName}${rankName}`;
 };
